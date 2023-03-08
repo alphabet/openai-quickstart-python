6636f593e8034621e0f222ab60c0a0dcd3565838
Gui Weinmann <gui.weinmann@consumer.org>
2023-03-08 16:58:16 -0500

    Working with a fine tune model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from models import Middelware
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -10,12 +11,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
+        form_input = request.form["animal"]
+        joey3 = Middelware.Joey3() # {'prompt': form_input}
+        response = joey3.talk()
+
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
