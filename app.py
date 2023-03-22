8302a7c42106b3af5fc7697b628f535f9a5c3557
Gui Weinmann <gui@alphabetware.com>
2023-03-22 09:09:02 -0400

    Working example

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
-from flask import Flask, session, redirect, render_template, request, url_for, jsonify
-# from flask_session import Session
+from flask import Flask, app, session, redirect, render_template, request, url_for, jsonify
+from flask_session import Session
 from models import Middleware
+from datetime import timedelta
 
 app = Flask(__name__)
-app.secret_key = "any random string"
-# Check Configuration section for more details
-# Session(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = "beelzebub coughed"
+
+
+sess = Session()
+#Session(app)
+
+@app.before_first_request
+def anonymous_function():
+  session.permanent = True
+  app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=5)
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
-    return render_template("index.html", result="")
+  return render_template("index.html")
 
 @app.route("/api", methods=(["GET"]))
 def api():
-    if not "talks" in session: session["talks"]=0
-    question = request.args.get("question")
-    joey3 = Middleware.Joey3(prompt = question)
-    joey3.talks = session["talks"]
-    response = joey3.talk()
-    session["talks"] += 1 
-    
-    return jsonify({'joey3': response, 'talks': joey3.talks})
+  if not "talks" in session: session["talks"]=0
+  question = request.args.get('question')
+  print("the question is >>>>>>>>>>>", question)
+  if question: question.replace("?", "") # and str(question).endswith("?")
+
+  joey3 = Middleware.Joey3(question = question)
+  joey3.talks = session["talks"]
+  response = joey3.talk()
+  session["talks"] += 1 
+  response['talks'] = session["talks"]
+  response['question'] = question
   
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+  return jsonify(response)
--- models/Middleware.py
+++ models/Middleware.py
@@ -14,36 +14,52 @@ class Joey3:
     def __init__(self, **kwargs):
         openai.api_key = os.getenv("OPENAI_API_KEY")
         engine = inflect.engine()
-        self.prompt = kwargs.get('prompt') or self.prompt
+        with open('./models/prompts/joey3.txt', 'r') as file:
+            try:
+                self.prompt = file.read()
+            finally:
+                file.close()
+
+        self.question = kwargs.get("question")
+        print(f">>>>>>>>>> question {self.question}")
+
         self.talks = kwargs.get("talks") or self.talks
         self.__limit = 9 # max number of interactions
-        self.__first_interaction = "Hi I'm Joey, an AI chatbot who knows a lot about the Upkept App and not much else. " \
+        self.__first_interaction = "Hi I'm Joey, an AI chatbot who knows a lot about the Upkept App but not much else. " \
                                    "What can I do for you?"
         self.__penultimate = f"Is there something else I can answer for you? I\'m only allowed to answer {self.__limit} " \
                              f"questions and this is our {engine.ordinal(self.__limit-1)} interaction."
-        print(self.__first_interaction)
-
- #       self.talk()
 
     def talk(self):
         self.talks += 1
-        print("talks: ", self.talks)
-        print("prompt: ", self.prompt)
+        print(">>>>> talks: ", self.talks)
+        print(">>>>> question: ", self.question)
+        print(">>>>> prompt: ", self.prompt)
 
         if(self.talks == self.__limit):
             return('\nBye! Hope you have a great day! For more answers please email us at upkept@cr.consumer.org')
 
-        question = self.prompt or "Hi"
+        question = self.question
         question_answer_completion = f"Q: {question}\nA:"
-        prompt = (f"The following is a conversation with our customer service AI. Our AI is nicknamed Joey. He answers questions about the Upkept App. Joey thinks customers would love to talk with a real person via email at \"upkept@cr.consumer.org\". A real person reads and responds to every email. Joey does not have a phone. Joey can not send a text message. \n\nJoey wants customers to download and install the app.\n\nOur app is available via the appstore links. Our app is called the \"Upkept App.\" The Upkept App helps homeowners manage your home like a pro. Upkept is your all-in-one app for maintaining your home inside and out. Upkept provides the maintenance plan, upkeep schedule, and reminders in the app. Add items to your \"Home Hub\". Most items take just minutes to add to your Home Hub. \n\nOur app has a 30-day free trial. After 30 days our app costs $4.99 per month. You can cancel anytime during or after the free trial.\n\nOur website is at \"https://upkepthome.com\". Joey ignores ambiguous questions.\n\nQ: Hi\nA: Hey, I'm Joey an AI chatbot who knows a lot about the Upkept App. What can I do for you?"
+        prompt = (f"{self.prompt}" 
                   f"\n\n"
                   f"{question_answer_completion}",
                   )
 
-        ai = openai.Completion.create(
+        print('*-*'*20,"\n\n")
+
+        if self.talks < 1: return {
+            'joey3': self.__first_interaction, 
+            'joey4': self.__first_interaction
+            }
+        if(self.talks == self.__limit-1): return {
+            'joey3': self.__penultimate, 
+            'joey4': self.__penultimate}
+
+        succinct = openai.Completion.create(
             model="curie:ft-nicky:upkept-succinct-q-and-a-2023-02-09-04-08-58",
             prompt=prompt,
-            temperature=0,
+            temperature=.75,
             max_tokens=128,
             top_p=1,
             frequency_penalty=1,
@@ -51,8 +67,20 @@ class Joey3:
             stop=["A:", "Q:", "\n"]
         )
 
-        self.prompt = "Q: "
+        tna = openai.Completion.create(
+            model="curie:ft-the-tomorrow-now-agency-2023-03-21-01-54-39",
+            prompt=prompt,
+            temperature=1,
+            max_tokens=128,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=0,
+            stop=["A:", "Q:", "\n"]
+        )
+            
 
-        if(self.talks == self.__limit-1): return (f"\n{self.__penultimate}")
-        return ai['choices'][0]['text'].strip()
+
+        return {'joey3': succinct['choices'][0]['text'].strip(), 
+                'joey4': tna['choices'][0]['text'].strip()
+                }
 
